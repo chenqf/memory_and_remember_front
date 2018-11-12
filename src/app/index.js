f067bcf738273d40d98fc5af7424b59b9692714b
chenqf <qifeng.chen1006@foxmail.com>
2018-11-12 18:11:56 +0800

    私有router 变更

--- src/app/index.js
+++ src/app/index.js
@@ -14,9 +14,10 @@ import Login from './user/login/index';
 import ErrorBoundary from './errorBoundary';
 import {ScrollToTop} from '../component/scroll/index';
 import {Modal} from 'antd-mobile';
+import withTime from '../hot/withTime';
 import 'react-fontawesome';
 import '../library/http';
-import './index.scss'
+import '../hot/index.scss'
 
 const supportsHistory = 'pushState' in window.history;
 const getConfirmation = (message, callback) => {
@@ -30,113 +31,25 @@ const getConfirmation = (message, callback) => {
     ]);
 };
 
-class Container extends Component{
-    constructor(props){
-        super(props);
-        this.everyTimeId = 0;
-        this.state = {
-            flg:true,
-            history:0,
-            start:Date.now(),
-            now:Date.now()
-        };
-        this.hiddenProperty = 'hidden' in document ? 'hidden' :
-            'webkitHidden' in document ? 'webkitHidden' :
-                'mozHidden' in document ? 'mozHidden' :
-                    null;
-        this.visibilityChangeEvent = this.hiddenProperty.replace(/hidden/i, 'visibilitychange');
-    }
-    addListener = ()=>{
-        this.setState({start:Date.now(),now:Date.now()});
-        this.everyTimeId = setInterval(()=>{
-            this.setState({now:Date.now()})
-        },10000);
-    };
-    removeListener = ()=>{
-        this.setState((prevState,props)=>{
-            return {history:prevState.history + prevState.now - prevState.start}
-        });
-        window.clearInterval(this.everyTimeId);
-    };
-    onVisibilityChange = ()=>{
-        if (!document[this.hiddenProperty]) {
-            this.addListener();
-            console.log('显示',Date.now());
-        }else{
-            this.removeListener();
-            console.log('关闭',Date.now());
-        }
-    };
-    componentDidMount(){
-        this.addListener();
-        document.addEventListener(this.visibilityChangeEvent, this.onVisibilityChange);
-    }
-    componentWillUnmount(){
-        window.clearInterval(this.everyTimeId);
-        document.removeEventListener(this.visibilityChangeEvent, this.onVisibilityChange);
-    }
-    change = ()=>{
-        this.setState((prevState,props)=>{
-            return {flg:!prevState.flg}
-        })
-    };
-    getTime = (time)=>{
-        let s = 1000,
-            m = 60 * s,
-            h = 60 * m,
-            d = 24 * h,
-            str = '';
-        if(time >= d){
-            str = ~~(time / d) + '天';
-            time = time % d;
-        }
-        if(time >= h){
-            str = ~~(time / h) + '小时';
-            time = time % h;
-        }
-        if(time >= m){
-            str = ~~(time / m) + '分钟';
-            time = time % m;
-        }
-        // if(time >= s){
-        //     str = ~~(time / s) + '秒';
-        //     time = time % s;
-        // }
-        return str ? str : '0分钟';
-    };
-    render(){
-        let time = this.getTime(this.state.history + this.state.now - this.state.start);
-        return (
-            <React.Fragment>
-                {
-                    this.state.flg ? <span className="ball" onClick={this.change}/> : <span onClick={this.change} className="ball ball-text">{time}</span>
-                }
-                {this.props.children}
-            </React.Fragment>
-        )
-    }
-}
+
 
 const App = () => (
-
         <Router
             forceRefresh={!supportsHistory}
             getUserConfirmation={getConfirmation} // 切换页面时，提示是否切换
         >
-            <Container>
-                <ScrollToTop>
-                    <ErrorBoundary>
-                        <Switch>
-                            {/*登录页面*/}
-                            <Route exact path="/user/login" component={Login} />
-                            {/*具有登录权限才可进入*/}
-                            <PrivateRoute path="/" component={Auth} />
-                        </Switch>
-                    </ErrorBoundary>
-                </ScrollToTop>
-            </Container>
+            <ScrollToTop>
+                <ErrorBoundary>
+                    <Switch>
+                        {/*登录页面*/}
+                        <Route exact path="/user/login" component={Login} />
+                        {/*具有登录权限才可进入*/}
+                        <PrivateRoute path="/" component={Auth} />
+                    </Switch>
+                </ErrorBoundary>
+            </ScrollToTop>
         </Router>
 );
 
 
-export default App;
+export default withTime(App);
---
+++ src/hot/withTime.js
@@ -0,0 +1,98 @@
+// @flow Created by 陈其丰 on 2018/11/6.
+import React,{PureComponent,Component} from 'react';
+import './index.scss'
+
+function getDisplayName(WrappedComponent) {
+    return WrappedComponent.displayName || WrappedComponent.name || 'Component';
+}
+
+export default (WrappedComponent)=> {
+    class WithTime extends Component{
+        constructor(props){
+            super(props);
+            this.everyTimeId = 0;
+            this.state = {
+                flg:true,
+                history:0,
+                start:Date.now(),
+                now:Date.now()
+            };
+            this.hiddenProperty = 'hidden' in document ? 'hidden' :
+                'webkitHidden' in document ? 'webkitHidden' :
+                    'mozHidden' in document ? 'mozHidden' :
+                        null;
+            this.visibilityChangeEvent = this.hiddenProperty.replace(/hidden/i, 'visibilitychange');
+        }
+        addListener = ()=>{
+            this.setState({start:Date.now(),now:Date.now()});
+            this.everyTimeId = setInterval(()=>{
+                this.setState({now:Date.now()})
+            },10000);
+        };
+        removeListener = ()=>{
+            this.setState((prevState,props)=>{
+                return {history:prevState.history + prevState.now - prevState.start}
+            });
+            window.clearInterval(this.everyTimeId);
+        };
+        onVisibilityChange = ()=>{
+            if (!document[this.hiddenProperty]) {
+                this.addListener();
+                console.log('显示',Date.now());
+            }else{
+                this.removeListener();
+                console.log('关闭',Date.now());
+            }
+        };
+        componentDidMount(){
+            this.addListener();
+            document.addEventListener(this.visibilityChangeEvent, this.onVisibilityChange);
+        }
+        componentWillUnmount(){
+            window.clearInterval(this.everyTimeId);
+            document.removeEventListener(this.visibilityChangeEvent, this.onVisibilityChange);
+        }
+        change = ()=>{
+            this.setState((prevState,props)=>{
+                return {flg:!prevState.flg}
+            })
+        };
+        getTime = (time)=>{
+            let s = 1000,
+                m = 60 * s,
+                h = 60 * m,
+                d = 24 * h,
+                str = '';
+            if(time >= d){
+                str = ~~(time / d) + '天';
+                time = time % d;
+            }
+            if(time >= h){
+                str = ~~(time / h) + '小时';
+                time = time % h;
+            }
+            if(time >= m){
+                str = ~~(time / m) + '分钟';
+                time = time % m;
+            }
+            // if(time >= s){
+            //     str = ~~(time / s) + '秒';
+            //     time = time % s;
+            // }
+            return str ? str : '0分钟';
+        };
+        render(){
+            let time = this.getTime(this.state.history + this.state.now - this.state.start);
+            return (
+                <React.Fragment>
+                    {
+                        this.state.flg ? <span className="ball" onClick={this.change}/> : <span onClick={this.change} className="ball ball-text">{time}</span>
+                    }
+                    <WrappedComponent/>
+                </React.Fragment>
+            )
+        }
+    }
+    WithTime.displayName = `WithTime(${getDisplayName(WrappedComponent)})`;
+    return WithTime
+}
