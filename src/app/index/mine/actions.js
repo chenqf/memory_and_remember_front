cefbb2b375d21c75b9a99cd8e2ea1f68a8ccb98f
chenqf <qifeng.chen1006@foxmail.com>
2018-12-19 11:07:55 +0800

    自定义redux 中间件，统一实现异步处理

--- src/app/index/mine/actions.js
+++ src/app/index/mine/actions.js
@@ -8,58 +8,21 @@ import {actions as wordAction } from '../../../store/entities/word/index';
 /*
  * 获取所有单词数量
  * */
-export const fetchAllCount = (params) => dispatch => {
-    dispatch(fetchAllCountRequest());
-    wordApi.queryAllCount(params).then(({count})=>{
-        dispatch(wordAction.updateAllCount(count));
-    }).catch((error)=>{
-        dispatch(fetchAllCountFailure(error));
-    });
-};
-
-/*发送请求，所有单词数量*/
-export const fetchAllCountRequest = () => ({
-    type:actionTypes.FETCH_ALL_COUNT_REQUEST
+export const fetchAllCount = params =>({
+    types:[actionTypes.FETCH_ALL_COUNT_REQUEST,actionTypes.FETCH_ALL_COUNT_SUCCESS,actionTypes.FETCH_ALL_COUNT_FAILURE],
+    params,
+    callAPI:wordApi.queryAllCount,
+    beforeDispatchSuccess:({count},dispatch)=>dispatch(wordAction.updateAllCount(count))
 });
 
-/*发送请求，所有单词数量 -成功*/
-export const fetchAllCountSuccess = (payload) => ({
-    type:actionTypes.FETCH_ALL_COUNT_SUCCESS,
-    payload
-});
-
-/*发送请求，所有单词数量 -失败*/
-export const fetchAllCountFailure = (payload) => ({
-    type:actionTypes.FETCH_ALL_COUNT_FAILURE,
-    payload
-});
 
 
 /*
- * 获取所有单词数量
+ * 获取所有疑难单词数量
  * */
-export const fetchHardCount = (params) => dispatch => {
-    dispatch(fetchHardCountRequest());
-    wordApi.queryHardCount(params).then(({count})=>{
-        dispatch(wordAction.updateHardCount(count));
-    }).catch((error)=>{
-        dispatch(fetchHardCountFailure(error));
-    });
-};
-
-/*发送请求，所有单词数量*/
-export const fetchHardCountRequest = () => ({
-    type:actionTypes.FETCH_HARD_COUNT_REQUEST
-});
-
-/*发送请求，所有单词数量 -成功*/
-export const fetchHardCountSuccess = (payload) => ({
-    type:actionTypes.FETCH_HARD_COUNT_SUCCESS,
-    payload
-});
-
-/*发送请求，所有单词数量 -失败*/
-export const fetchHardCountFailure = (payload) => ({
-    type:actionTypes.FETCH_HARD_COUNT_FAILURE,
-    payload
+export const fetchHardCount = params =>({
+    types:[actionTypes.FETCH_HARD_COUNT_REQUEST,actionTypes.FETCH_HARD_COUNT_SUCCESS,actionTypes.FETCH_HARD_COUNT_FAILURE],
+    params,
+    callAPI:wordApi.queryHardCount,
+    beforeDispatchSuccess:({count},dispatch)=>dispatch(wordAction.updateHardCount(count))
 });
